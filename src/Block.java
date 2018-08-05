1ff787bff6ab10b7659ba37d1cd4a84cca1af144
Olexiy Koval <kalex32@ukr.net>
2018-08-06 02:05:47 +0300

    Create block

---
+++ src/Block.java
@@ -0,0 +1,11 @@
+class Block {
+    private double length;
+    private double width;
+    private double height;
+
+    Block(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+}
--- src/Menu.java
+++ src/Menu.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 class Menu {
     void menu() {
         System.out.println("Сделайте выбор:" + "\n" +
                 "1. Введите блок" + "\n" +
                 "2. Введите сферу" + "\n" +
                 "3. Введите пирамиду" + "\n" +
-                "4. Показать данные"+"\n"+
+                "4. Показать данные" + "\n" +
                 "5. Выход");
-    }
 
+        Scanner scanner = new Scanner(System.in);
+        try {
+            switch (scanner.nextInt()) {
+                case 1:
+                    // Перенести в отдельный метод в класс Блок
+                    //System.out.println("Введите длину, ширину и высоту блока");
+                    //Block block = new Block(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+            }
+        } catch (ArrayStoreException e){
+            menu();
+        }
+
+    }
 }
