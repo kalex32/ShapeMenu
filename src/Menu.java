2c3975a686549a0ef2ce1144d778e45f21cd2d71
Olexiy Koval <kalex32@ukr.net>
2018-08-18 00:34:22 +0300

    operator break+ add cycle if

--- src/Menu.java
+++ src/Menu.java
@@ -6,42 +6,42 @@ class Menu {
     ArrayList<Shapes> arrayListsShapes = new ArrayList<>();
 
     void menu() {
-        System.out.println("Сделайте выбор:" + "\n" +
-                "1. Введите блок" + "\n" +
-                "2. Введите сферу" + "\n" +
-                "3. Введите пирамиду" + "\n" +
-                "4. Показать данные" + "\n" +
-                "5. Выход");
+        if (arrayListsShapes.size() < 10) {
+            System.out.println("Сделайте выбор:" + "\n" +
+                    "1. Введите блок" + "\n" +
+                    "2. Введите сферу" + "\n" +
+                    "3. Введите пирамиду" + "\n" +
+                    "4. Показать данные" + "\n" +
+                    "5. Выход");
 
-        Scanner scanner = new Scanner(System.in);
-        switch (scanner.nextInt()) {
-            case 1:
-                Block block = new Block();
-                arrayListsShapes.add(block.createShapes());
-                menu();
-                //break;
-            case 2:
-                Sphera sphera = new Sphera();
-                arrayListsShapes.add(sphera.createShapes());
-                menu();
-                //break;
-            case 3:
-                Pyramid pyramid = new Pyramid();
-                arrayListsShapes.add(pyramid.createShapes());
-                menu();
-                //break;
-            case 4:
-                for (Shapes s : arrayListsShapes) System.out.println(s);
-                System.out.println();
-                menu();
-            case 5:
-                break;
-            default:
-                menu();
-
-        }
+            Scanner scanner = new Scanner(System.in);
+            switch (scanner.nextInt()) {
+                case 1:
+                    Block block = new Block();
+                    arrayListsShapes.add(block.createShapes());
+                    menu();
+                    //break;
+                case 2:
+                    Sphera sphera = new Sphera();
+                    arrayListsShapes.add(sphera.createShapes());
+                    menu();
+                    //break;
+                case 3:
+                    Pyramid pyramid = new Pyramid();
+                    arrayListsShapes.add(pyramid.createShapes());
+                    menu();
+                    //break;
+                case 4:
+                    for (Shapes s : arrayListsShapes) System.out.println(s);
+                    System.out.println();
+                    menu();
+                case 5:
+                    break;
+                default:
+                    menu();
+            }
+        }else return;
     }
-
 }
 
 
